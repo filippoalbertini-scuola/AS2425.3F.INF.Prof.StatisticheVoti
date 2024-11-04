21b12498dbe09ac778945cd1dc0f5554af8d71ac
Filippo Albertini <filippo@filippoalbertini.it>
2024-11-04 08:49:55 +0100

    Aggiungere i file di progetto.

---
+++ AS2425.3F.INF.Prof.StatisticheVoti/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace AS2425._3F.INF.Prof.StatisticheVoti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prof;3F;04/11/24;Statistiche voti");
+
+            Random r = new Random();
+            long voto;
+
+            Console.WriteLine("Digita il numero di studenti della prova");
+            int nStudenti = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nStudenti; i++)
+            {
+                voto = r.Next(1, 11);
+                Console.WriteLine($"{i} {voto}");
+            }
+
+        }
+    }
+}
